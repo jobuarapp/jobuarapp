29f84bcf9d590a228dd3c11e11694adb20cce7cb
Rayas Abdul Rahiman <58700409+rayasrahiman@users.noreply.github.com>
2021-05-25 18:13:44 +0530

    'files'

--- screens/OtpScreen.js
+++ screens/OtpScreen.js
@@ -1,17 +1,48 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	Image,
+	Dimensions,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from 'react-native';
 import Colors from '../constants/Colors';
+import OTPTextView from 'react-native-otp-textinput';
 
-const OtpScreen = () => {
+const OtpScreen = (props) => {
+	const [otp, setOtp] = useState('');
+	const [otpInput, setOtpInput] = useState('');
 	return (
-		<View style={styles.container}>
-			<View style={styles.logoContainer}>
-				<Image source={require('../assets/jobuar-logo.jpg')} style={styles.logo} resizeMode="contain" />
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} style={{ flex: 1 }}>
+			<View style={styles.container}>
+				<View style={styles.logoContainer}>
+					<Image source={require('../assets/jobuar-logo.jpg')} style={styles.logo} resizeMode="contain" />
+				</View>
+				<View style={styles.verifyTxtContainer}>
+					<Text style={styles.verifyTxt}>Verification code has been sent to you on your phone number</Text>
+				</View>
+				<OTPTextView
+					ref={(e) => setOtp(e)}
+					containerStyle={styles.textInputContainer}
+					handleTextChange={(text) => setOtpInput(text)}
+					inputCount={5}
+					keyboardType="numeric"
+					tintColor={Colors.mainText}
+				/>
+				<TouchableOpacity onPress={() => props.navigation.navigate('Otp')} style={styles.doneBtnContainer}>
+					<Text style={styles.doneBtnTxt}>Done</Text>
+				</TouchableOpacity>
+				<View style={styles.verifyRSNDCodeContainer}>
+					<Text style={styles.verifyTxt}>Didn't get verification code?</Text>
+					<TouchableOpacity>
+						<Text style={styles.verifyRSNDCodeTxt}>Resend</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
-			<View style={styles.verifyTxtContainer}>
-				<Text style={styles.verifyTxt}>Verification code has been sent to you on your phone number</Text>
-			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
@@ -36,6 +67,28 @@ const styles = StyleSheet.create({
 		color: Colors.inActive,
 		fontSize: 14,
 		fontFamily: 'poppins-regular',
+		textAlign: 'center',
+	},
+	doneBtnContainer: {
+		backgroundColor: Colors.mainBackg,
+		borderRadius: 25,
+		width: Dimensions.get('window').width * 0.9,
+		alignItems: 'center',
+		marginVertical: Dimensions.get('window').height * 0.02,
+	},
+	doneBtnTxt: {
+		color: Colors.mainText,
+		padding: 15,
+		fontSize: 16,
+		fontFamily: 'poppins-bold',
+	},
+	// verifyRSNDCodeContainer:{
+
+	// },
+	verifyRSNDCodeTxt: {
+		color: Colors.inActive,
+		fontSize: 16,
+		fontFamily: 'poppins-bold',
 		textAlign: 'center',
 	},
 });
